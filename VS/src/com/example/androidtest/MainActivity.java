6ad1404f56c90dbd1765acfb10d9ffa4b06c5b9e
Tiger <kuroneko59@icloud.com>
2018-09-18 15:35:05 +0900

    スタート画面変更しました

--- VS/src/com/example/androidtest/MainActivity.java
+++ VS/src/com/example/androidtest/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.androidtest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button STARTButton = (Button)findViewById(R.id.button0);
+		STARTButton.setOnClickListener(new OnClickListener() {
+		public void onClick(View v){
+			Intent intent = new Intent(MainActivity.this,CourseActivity.class);
+			startActivity(intent);
+		}
+		});
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
